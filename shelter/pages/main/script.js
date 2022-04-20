9306133c48595154adec83f20af07c2fd110d593
Nina Ponomareva <nina.9412@mail.ru>
2022-04-21 01:01:26 +0300

    feat: close burger navigation by click on free area

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -4,6 +4,7 @@ const burgerButton = document.querySelector('.burger-button');
 const navigationPanel = document.querySelector('.navigation-panel');
 const header = document.querySelector('.header');
 let menuLinks = document.querySelectorAll('.nav-link');
+let mainNav = document.querySelector('.main-nav');
 
 burgerButton.addEventListener('click', () => {
     burgerButton.classList.toggle('burger-button_active');
@@ -20,3 +21,18 @@ menuLinks.forEach(menuLink => {
     header.classList.remove('header_active');
     });
 });
+
+window.addEventListener('click', elem => {
+    if (navigationPanel.classList.contains('navigation-panel_active')) {
+        const target = elem.target;
+            
+        if (!target.closest('.navigation-panel') && (!target.closest('.burger-button'))) {
+            burgerButton.classList.remove('burger-button_active');
+            navigationPanel.classList.remove('navigation-panel_active')
+            document.body.classList.remove('no-scroll');
+            header.classList.remove('header_active');
+        }
+    }    
+});
+
+
