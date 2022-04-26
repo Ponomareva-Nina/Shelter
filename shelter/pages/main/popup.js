ae7b46a88e65c6906114f64b6b62cb878217384a
Nina Ponomareva <nina.9412@mail.ru>
2022-04-26 20:34:11 +0300

    feat: add basic popup behaviour

--- shelter/pages/main/popup.js
+++ shelter/pages/main/popup.js
@@ -1,2 +1,41 @@
 "use strict"
 
+const cards = document.querySelectorAll('.card');
+const popupCloseBtn = document.querySelector('.popup-close');
+const body = document.querySelector('body');
+const popup = document.querySelector('.popup');
+
+
+
+cards.forEach(card => {
+    card.addEventListener('click', popupToggle);
+});
+
+popupCloseBtn.addEventListener('click', popupClose);
+
+
+// === закрытие попапа на клик по свободной области: ===
+popup.addEventListener('click', elem => {
+    if (popup.classList.contains('popup_open')) {
+        const target = elem.target;
+        console.log(target);
+    
+        if (target.closest('body') && (!target.closest('.popup-close')) && (!target.closest('.popup__wrapper'))) {
+            popupClose();
+        }
+    }    
+});
+
+function popupToggle() {
+    popup.classList.toggle('popup_open');
+    document.body.classList.toggle('no-scroll');
+}
+
+function popupClose(){
+    popup.classList.remove('popup_open');
+    document.body.classList.remove('no-scroll');
+}
+
+function findCard() {
+    
+}
